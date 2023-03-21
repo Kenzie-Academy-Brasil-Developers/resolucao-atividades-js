5ca96f16b633ddd0210fece1da2c1a3b577894c5
alessandravicentini <alessandravicentini01@gmailc.om>
2023-03-21 10:25:18 -0300

    feat: resolução desafio portaria

--- desafio_portaria/index.js
+++ desafio_portaria/index.js
@@ -1,1 +1,19 @@
 // Desafio - JavaScript: Desafio Portaria
+
+let nome = prompt('Digite seu nome')
+let idade = Number(prompt('Digite sua idade'))
+let estaAcompanhado = prompt('Você está acompanhado? Digite "s" para sim e "n" para não')
+
+if (estaAcompanhado.toLowerCase() == 's'){
+    estaAcompanhado = true
+} else {
+    estaAcompanhado = false
+}
+
+if (idade < 18){
+    alert(`Entrada não permitida para ${nome}: Menor de idade`)
+} else if (idade >= 18 && estaAcompanhado){
+    alert(`Entrada permitida para ${nome}: Conceder desconto`)
+} else {
+    alert(`Entrada permitida para ${nome}: Valor integral`)
+}
