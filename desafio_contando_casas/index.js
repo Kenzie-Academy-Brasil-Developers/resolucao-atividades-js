ff591eaddeba19bcfb9ec1fa36172c2e050d5bd6
luiz <luizpauloreiscardosoo@gmail.com>
2023-03-21 11:19:37 -0300

    Desafio-contando-casas done

--- desafio_contando_casas/index.js
+++ desafio_contando_casas/index.js
@@ -1,1 +1,21 @@
 // Desafio - JavaScript: Contando Casas
+function gerarNumeroAleatorio() {
+  return Math.floor(Math.random() * 20) + 1;
+}
+
+function verificarPalpite(numeroAleatorio, palpite) {
+  let tentativa = 1;
+
+  while (numeroAleatorio !== palpite) {
+    alert("Tente novamente");
+    palpite = parseInt(prompt("Digite o seu palpite (número entre 1 e 20):"));
+    tentativa++;
+  }
+
+  alert(`Você acertou na tentativa ${tentativa}.`);
+}
+
+const numeroAleatorio = gerarNumeroAleatorio();
+let palpite = parseInt(prompt("Digite o seu palpite (número entre 1 e 20):"));
+
+verificarPalpite(numeroAleatorio, palpite);
