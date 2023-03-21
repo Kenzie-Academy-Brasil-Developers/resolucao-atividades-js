23d1c7eb33504b3e516dc5dfaef27306cdeb4974
thiagoKenzie <102174562+thiagoKenzie@users.noreply.github.com>
2023-03-21 14:47:16 -0300

    Update index.js

--- desafio_enigma/index.js
+++ desafio_enigma/index.js
@@ -1,1 +1,18 @@
 // Desafio - JavaScript: Enigma
+function countWords(word){
+    let count = 0;
+    if(word[0] != word[0].toUpperCase()){
+        count = 1;
+    }
+
+    let flag = false;
+    for(let i = 0; i<word.length; i++){
+        if(word[i] == word[i].toUpperCase()){
+            flag = true;
+        }else if(word[i] != word[i].toUpperCase() && flag){
+            flag = false;
+            count++
+        } 
+    }
+    return count;
+}
