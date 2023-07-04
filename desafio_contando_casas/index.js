e9a01f33262de917e0309ef05842c8653ee27838
Kayan Chagas <67170002+zkayan@users.noreply.github.com>
2023-07-04 11:46:40 -0300

    Update Desafio Contando Casas

--- desafio_contando_casas/index.js
+++ desafio_contando_casas/index.js
@@ -1,11 +1,15 @@
 // Desafio - JavaScript: Contando Casas
+
+// Função para gerar um número aleatório entre 1 e 20
 function gerarNumeroAleatorio() {
   return Math.floor(Math.random() * 20) + 1;
 }
 
+// Função para verificar o palpite do usuário em relação ao número aleatório
 function verificarPalpite(numeroAleatorio, palpite) {
   let tentativa = 1;
 
+  // Enquanto o número aleatório for diferente do palpite do usuário, continua pedindo mais palpites
   while (numeroAleatorio !== palpite) {
     alert("Tente novamente");
     palpite = parseInt(prompt("Digite o seu palpite (número entre 1 e 20):"));
@@ -15,7 +19,11 @@ function verificarPalpite(numeroAleatorio, palpite) {
   alert(`Você acertou na tentativa ${tentativa}.`);
 }
 
+// Gera um número aleatório
 const numeroAleatorio = gerarNumeroAleatorio();
+
+// Solicita ao usuário que digite um palpite
 let palpite = parseInt(prompt("Digite o seu palpite (número entre 1 e 20):"));
 
+// Chama a função verificarPalpite passando o número aleatório e o palpite do usuário como argumentos
 verificarPalpite(numeroAleatorio, palpite);
