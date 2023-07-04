a3610d82a5f6fd12915e9965206f60be7752a3e6
Kayan Chagas <67170002+zkayan@users.noreply.github.com>
2023-07-04 12:35:08 -0300

    Update Desafio Dados Escolares

--- desafio_dados_escolares/index.js
+++ desafio_dados_escolares/index.js
@@ -9,16 +9,20 @@ const alunoCurso = {
   situacaoMatricula: true,
 };
 
+// Solicita ao usuário a opção de consulta
 const tipoConsulta = prompt('Digite a opção da consulta. (1 - Curso | 2 - Matéria | 3 - Situação de matrícula | 4 - Gerar carteirinha')
 
+// Função para obter o curso do aluno
 function getTheCourse(aluno){
     alert(aluno.curso)
 }
 
+// Função para obter as matérias do aluno
 function getTheSubjects(aluno){
     alert(aluno.materias)
 }
 
+// Função para obter a situação de matrícula do aluno
 function registrationStatus(aluno){
     if (aluno.situacaoMatricula){
         aluno.situacaoMatricula = 'Ativo'
@@ -29,12 +33,14 @@ function registrationStatus(aluno){
     alert(aluno.situacaoMatricula)
 }
 
+// Função para gerar a carteirinha do aluno
 function generateStudentCard(aluno){
     const cardInformation = `name: ${aluno.nome}, idade: ${aluno.idade}, curso: ${aluno.curso}, instituição: ${aluno.instituicao}`
 
     alert(cardInformation)
 }
 
+// Verifica a opção de consulta escolhida pelo usuário
 if (tipoConsulta == 1){
     getTheCourse(alunoCurso)
 } else if (tipoConsulta == 2){
